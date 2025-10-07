81ea81a76442d9b9016064725f0ba7370b1e8203
MarkMartinez199504 <ruinslasher2@gmail.com>
2025-10-07 17:45:39 -0400

    finished basic functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
 from stats import count_words
 from stats import count_chars
 from stats import sort_dic
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -9,14 +11,17 @@ def get_book_text(path):
     return contents
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    word_count = count_words(get_book_text("books/frankenstein.txt"))
+    word_count = count_words(get_book_text(sys.argv[1]))
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
 
-    char_count = count_chars(get_book_text("books/frankenstein.txt"))
+    char_count = count_chars(get_book_text(sys.argv[1]))
 
     sorted_chars = sort_dic(char_count)
 
